a29c05eed435d622c6e6d548f7595246910ef4f6
Sameer <gautam.mudawadkar20@vit.edu>
2023-05-21 14:49:45 +0530

    FirstCommit

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    sepolia: {
+      url: "https://eth-sepolia.g.alchemy.com/v2/UDVZhyhbo8ix99u9Qoz-1zoqrd24l684",
+      accounts: ["a0182f827bbae65df9dbbd52d8ca404d451c8428719da4d580768218491c988b","6234b5401d346e58f492e5e1f78dd4e0bb4e1d45a478251cef668648fe7bceea"]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
